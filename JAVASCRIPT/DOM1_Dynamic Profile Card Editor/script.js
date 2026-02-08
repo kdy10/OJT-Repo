2b296355dbfa152129215481507cecd64e57fd5f
kdy10 <krishae.delacruz@clsu2.edu.ph>
2026-02-08 11:08:39 +0800

    fix bg color

--- JAVASCRIPT/DOM1_Dynamic Profile Card Editor/script.js
+++ JAVASCRIPT/DOM1_Dynamic Profile Card Editor/script.js
@@ -1,7 +1,7 @@
 const profilePicture = document.getElementById('profile-picture');
 const profileName = document.getElementById('profile-name');
 const profileBio = document.getElementById('profile-bio');
-
+const profileCard = document.getElementById('profile-card');    
 
 function updateProfileName() {      // updates the name based on input
     const newName = document.getElementById("name").value.trim();
@@ -23,14 +23,8 @@ function updateProfileImage() {      // updates the profile image based on input
     profilePicture.setAttribute('src', newImageUrl);
 }
 
-function updateBgColor() {     // updates the bg color randomly
+function updateBgColor() {          // updates the bg color of the profile card based on input
     const newColor = document.getElementById("color").value;
-    const profileCard = document.getElementById('profile-card');
 
-    const red =  Math.floor(Math.random() * 256);
-    const green = Math.floor(Math.random() * 256);
-    const blue = Math.floor(Math.random() * 256);
-    const randomBgColor = "rgb(" + red + ", " + green + ", " + blue + ")";
-    
-    profileCard.style.backgroundColor = randomBgColor;
+    profileCard.style.backgroundColor = newColor;
 }
