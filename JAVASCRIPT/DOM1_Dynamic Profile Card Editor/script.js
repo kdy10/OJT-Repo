add3e33b6fe4485c42320ab7d72ee3f63e997840
kdy10 <krishae.delacruz@clsu2.edu.ph>
2026-02-08 11:02:46 +0800

    Update script.js

--- JAVASCRIPT/DOM1_Dynamic Profile Card Editor/script.js
+++ JAVASCRIPT/DOM1_Dynamic Profile Card Editor/script.js
@@ -3,27 +3,27 @@ const profileName = document.getElementById('profile-name');
 const profileBio = document.getElementById('profile-bio');
 
 
-function updateProfileName() {
+function updateProfileName() {      // updates the name based on input
     const newName = document.getElementById("name").value.trim();
     if (newName) {
         profileName.textContent = newName;
     }
 }
 
-function updateProfileBio() {
+function updateProfileBio() {       // updates the bio based on input
     const newBio = document.getElementById("bio").value.trim();
     if (newBio) {
         profileBio.textContent = newBio;
     }
 }
 
-function updateProfileImage() {
+function updateProfileImage() {      // updates the profile image based on input    
     const newImageUrl = document.getElementById("image").value.trim();
     
     profilePicture.setAttribute('src', newImageUrl);
 }
 
-function updateBgColor() {
+function updateBgColor() {     // updates the bg color randomly
     const newColor = document.getElementById("color").value;
     const profileCard = document.getElementById('profile-card');
 
